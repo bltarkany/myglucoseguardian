8f2f574be15d605865678793a16bb61000824301
Max <mhandler1991@gmail.com>
2019-11-17 16:42:48 -0800

    front page update

--- client/src/App/components/Content/Content.js
+++ client/src/App/components/Content/Content.js
@@ -13,12 +13,12 @@ class Content extends Component {
         <Row className="d-flex justify-content-between">
           {contentData.map((col, i) => (
             <Col key={i} md={5} className="mb-4">
-              <h6 className="mb-3">
+              <h4 className="mb-3">
                 <a href={col.link}>
-                  <FontAwesomeIcon icon="link" className="mr-2" />
+                  {/* <FontAwesomeIcon icon="link" className="mr-2" /> */}
                   {col.title}
                 </a>
-              </h6>
+              </h4>
               <p>{col.description}</p>
             </Col>
           ))}
--- client/src/App/components/Hero/Hero.js
+++ client/src/App/components/Hero/Hero.js
@@ -1,15 +1,15 @@
 import React from "react";
 
-import logo from "../../assets/logo.svg";
+// import logo from "../../assets/logo.svg";
 
 const Hero = () => (
   <div className="text-center hero my-5">
-    <img className="mb-3 app-logo" src={logo} alt="React logo" width="120" />
-    <h1 className="mb-4">React.js Sample Project</h1>
+    <img className="mb-3 app-logo" src="cartoon.jpg" alt="logo" width="120" />
+    <h1 className="mb-4">Glucose Guardian</h1>
 
     <p className="lead">
-      This is a sample application that demonstrates an authentication flow for
-      an SPA, using <a href="https://reactjs.org">React.js</a>
+    Parents &amp; Children learn how to better manage your diabetes through Education and Games!
+    {/* <a href="https://reactjs.org">React.js</a> */}
     </p>
   </div>
 );
