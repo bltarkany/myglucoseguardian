915d5c64388738629eb00c307dfc4950e4850ff6
Bobbi Tarkany <bl.tarkany@gmail.com>
2019-11-21 18:28:22 -0800

    Updated setupProxy.js

--- client/src/directory/setupProxy.js
+++ client/src/directory/setupProxy.js
@@ -1,8 +1,8 @@
-const proxy = require('http-proxy-middleware');
+const proxy = require("http-proxy-middleware");
 
 module.exports = function(app) {
-    app.use(
-        // eslint-disable-next-line no-sparse-arrays
-        proxy(["/api/", , "/otherApi"], {target: "http://localhost:5000"})
-    );
+  app.use(
+    // eslint-disable-next-line no-sparse-arrays
+    proxy(["/api", , "/otherApi"], { target: "http://localhost:5000" })
+  );
 };
