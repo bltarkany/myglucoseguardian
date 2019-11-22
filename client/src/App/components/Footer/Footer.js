6b62e5213206fac6adef8833d29d6207160f1a5d
Max <mhandler1991@gmail.com>
2019-11-21 21:15:38 -0800

    css

--- client/src/App/components/Footer/Footer.js
+++ client/src/App/components/Footer/Footer.js
@@ -1,9 +1,9 @@
 import React from "react";
 import Icon from "../Icon/Icon"
 import "./Footer.css"
-
+ 
 const Footer = () => (
-  <footer className="bg-light p-3 text-center footer">
+  <footer className="p-3 text-center" style={{backgroundColor: "#662E9B"}}>
     <Icon/>
     <p>
       Glucose Guardian<a href="https://github.com/bltarkany/myglucoseguardian/tree/master/client/src/App"> GitHub</a>
