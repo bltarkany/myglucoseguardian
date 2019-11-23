fda7f76f87714afa5d4638de149d83a05b0e4151
Max <mhandler1991@gmail.com>
2019-11-23 15:57:36 -0800

    General Fixes

--- client/src/App/components/Navigation/Navigation.jsx
+++ client/src/App/components/Navigation/Navigation.jsx
@@ -2,24 +2,24 @@ import React from "react";
 
 const Navigation = () => {
     return (
-        <ul className = "nav nav-tabs justify-content-center bg-light border" style = {{width: "100%", textAlign: "center"}}>
+        <ul className = "nav nav-tabs justify-content-center" style = {{width: "100%", textAlign: "center", backgroundColor: "#5AC0FD"}}>
             <li className = "nav-item">
-                <a className = "nav-link" href = "/n" style = {{fontFamily: "Source Sans Pro, sans-serif"}}>
+                <a className = "nav-link" href = "/summary" style = {{fontFamily: "Source Sans Pro, sans-serif"}}>
                     <h4 style = {{color:"black"}}>Summary</h4>
                 </a>
             </li>
             <li className = "nav-item">
-                <a className = "nav-link" href = "/n" style = {{fontFamily: "Source Sans Pro, sans-serif"}}>
+                <a className = "nav-link" href = "/glucose" style = {{fontFamily: "Source Sans Pro, sans-serif"}}>
                     <h4 style = {{color:"black"}}>Glucose</h4>
                 </a>
             </li>
             <li className = "nav-item">
-                <a className = "nav-link" href = "/n" style = {{fontFamily: "Source Sans Pro, sans-serif"}}>
+                <a className = "nav-link" href = "/food" style = {{fontFamily: "Source Sans Pro, sans-serif"}}>
                     <h4 style = {{color:"black"}}>Food</h4>
                 </a>
             </li>
             <li className = "nav-item">
-                <a className = "nav-link" href = "/n" style = {{fontFamily: "Source Sans Pro, sans-serif"}}>
+                <a className = "nav-link" href = "/calendar" style = {{fontFamily: "Source Sans Pro, sans-serif"}}>
                     <h4 style = {{color:"black"}}>Calendar</h4>
                 </a>
             </li>
