958cd985b7ceb4f7f3e1f98ce71c29ff50572bc9
Luke DeLeon <lukedeleon94@gmail.com>
2019-11-23 11:01:42 -0800

    mySummary import fix

--- client/src/App/components/Content/Content.jsx
+++ client/src/App/components/Content/Content.jsx
@@ -8,26 +8,28 @@ import contentData from "../../utils/contentData";
 // Comment
 
 class Content extends Component {
-  render() {
-    return (
-      <div className="next-steps my-5">
-        <h2 className="my-5 text-center" style={{color: "#FF3849"}}>We Provide a place for education ... and lots of fun!</h2>
-        <Row className="d-flex justify-content-between">
-          {contentData.map((col, i) => (
-            <Col key={i} md={5} className="mb-4">
-              <h3 className="mb-3">
-                <a href={col.link} style={{color: "#662E9B"}}>
-                  {/* <FontAwesomeIcon icon="link" className="mr-2" /> */}
-                  {col.title}
-                </a>
-              </h3>
-              <p>{col.description}</p>
-            </Col>
-          ))}
-        </Row>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="next-steps my-5">
+                <h2 className="my-5 text-center" style={{ color: "#FF3849" }}>
+                    We Provide a place for education ... and lots of fun!
+                </h2>
+                <Row className="d-flex justify-content-between">
+                    {contentData.map((col, i) => (
+                        <Col key={i} md={5} className="mb-4">
+                            <h3 className="mb-3">
+                                <a href={col.link} style={{ color: "#662E9B" }}>
+                                    {/* <FontAwesomeIcon icon="link" className="mr-2" /> */}
+                                    {col.title}
+                                </a>
+                            </h3>
+                            <p>{col.description}</p>
+                        </Col>
+                    ))}
+                </Row>
+            </div>
+        );
+    }
 }
 
 export default Content;
